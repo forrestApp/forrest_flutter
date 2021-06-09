5330db5c2256a9194402885f8efa41524afe4d55
Esther Kummetz <estherkummetz@Esthers-MBP.local>
2021-06-09 12:55:39 +0200

    login with design

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import UIKit
 import Flutter
-
+//import google
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -8,6 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    //GMSServices.provideAPIKey(AIzaSyBLmpxMi1LTBieITR4dWMFqFtCGzQ57b08)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/screens/home/map_local_alternative.dart
+++ lib/screens/home/map_local_alternative.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:forrest_flutter/screens/authenticate/sign_in.dart';
 import 'package:forrest_flutter/screens/home/profileSettings.dart';
+//import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapLocalAlternative extends StatefulWidget {
   @override
@@ -20,6 +21,14 @@ class _MapLocalAlternativeState extends State<MapLocalAlternative> {
         break;
     }
   }
+
+  /*GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -88,7 +97,6 @@ class _MapLocalAlternativeState extends State<MapLocalAlternative> {
         padding: EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(height: 20.0),
             Text(
@@ -104,6 +112,13 @@ class _MapLocalAlternativeState extends State<MapLocalAlternative> {
               height: 60.0,
               color: Colors.lightGreen[500],
             ),
+            /*GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+            ),*/
           ],
         ),
       ),
